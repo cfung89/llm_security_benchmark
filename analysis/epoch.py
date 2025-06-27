8c9098c5d2e2ca43daa8538d18a9660415175a36
Cyrus <cyhfung@gmail.com>
2025-06-26 23:28:38 -0400

    fix: bootstrapping

--- analysis/epoch.py
+++ analysis/epoch.py
@@ -1,6 +1,6 @@
 #! ../.venv/bin/python3
 
-import os
+import os, sys
 from inspect_ai.log import read_eval_log, read_eval_log_sample_summaries
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,7 +9,7 @@ import pandas as pd
 from utils import _get_model_name, _get_task_name
 
 num_of_epochs = 10
-data_path = "../data"
+data_path = "../logs"
 
 def extract() -> dict:
     out = {}
@@ -32,7 +32,7 @@ def extract() -> dict:
             score = _get_score(s)
             task_output[s.epoch-1] += score
             df.at[s.id, s.epoch] = int(score)
-            print(df.head)
+            # print(df.head)
             # print(s.id, s.epoch, s.scores["includes"].value)
         out[name] = (task_output, header.eval.dataset.samples)
         new_name = i.split("/")[-1].replace(".eval", ".csv")
@@ -40,7 +40,7 @@ def extract() -> dict:
         mean_row = df.mean()
         df.iloc[-1] = mean_row
         # df.iloc[-1, -1] = df.iloc[:-1, :-1].mean().mean()
-        print(df.head)
+        # print(df.head)
         df.to_csv(f"../results/{new_name}")
     return out
 
@@ -61,47 +61,52 @@ def line_plot(data: dict) -> None:
 
     for name, d in list(data.items()):
         task_name, model_name = name.split("/")
-        ci_lower3, ci_upper3 = _compute_ci(d[0])
-        ci_lower4, ci_upper4 = _compute_ci(d[1])
+        # _compute_ci(d[0])
+        # _compute_ci(d[1])
+        means, stdevs = _compute_mean(name)
+        mean_stds, ci_lower4, ci_upper4 = _compute_ci_bootstrap(name)
+        ci_lower3, ci_upper3 = _compute_ci(means, stdevs)
         colour = ("g", "green") if "mistral" in model_name.lower() else ("b", "blue")
         if task_name == "cybench":
-            cybench1.plot(x, d[0], f"{colour[0]}o-", label=model_name)
-            cybench2.plot(x, d[1], f"{colour[0]}o-", label=model_name)
+            cybench1.plot(x, means, f"{colour[0]}o-", label=model_name)
+            cybench2.plot(x, stdevs, f"{colour[0]}o-", label=model_name)
 
-            cybench3.plot(x, d[0], f"{colour[0]}o-", label=model_name)
+            cybench3.plot(x, means, f"{colour[0]}o-", label=model_name)
             cybench3.fill_between(x, ci_lower3, ci_upper3, color=f"light{colour[1]}")
 
-            cybench4.plot(x, d[1], f"{colour[0]}o-", label=model_name)
+            # cybench4.plot(x, d[1], f"{colour[0]}o-", label=model_name)
+            # cybench4.fill_between(x, d[1] - ci_lower4, d[1] + ci_upper4, color=f"light{colour[1]}")
+            cybench4.plot(x, stdevs, f"{colour[0]}o-", label=model_name)
             cybench4.fill_between(x, ci_lower4, ci_upper4, color=f"light{colour[1]}")
         elif task_name == "gdm_intercode_ctf":
-            intercode1.plot(x, d[0], f"{colour[0]}o-", label=model_name)
-            intercode2.plot(x, d[1], f"{colour[0]}o-", label=model_name)
+            intercode1.plot(x, means, f"{colour[0]}o-", label=model_name)
+            intercode2.plot(x, stdevs, f"{colour[0]}o-", label=model_name)
 
-            intercode3.plot(x, d[0], f"{colour[0]}o-", label=model_name)
+            intercode3.plot(x, means, f"{colour[0]}o-", label=model_name)
             intercode3.fill_between(x, ci_lower3, ci_upper3, color=f"light{colour[1]}")
 
-            intercode4.plot(x, d[1], f"{colour[0]}o-", label=model_name)
+            intercode4.plot(x, stdevs, f"{colour[0]}o-", label=model_name)
             intercode4.fill_between(x, ci_lower4, ci_upper4, color=f"light{colour[1]}")
         else:
             raise ValueError(f"Invalid model name: {model_name}")
 
-    cybench1.set_xlabel("Epochs")
-    intercode1.set_xlabel("Epochs")
-    cybench2.set_xlabel("Epochs")
-    intercode2.set_xlabel("Epochs")
-    cybench3.set_xlabel("Epochs")
-    intercode3.set_xlabel("Epochs")
-    cybench4.set_xlabel("Epochs")
-    intercode4.set_xlabel("Epochs")
+    cybench1.set_xlabel("Number of attempts")
+    intercode1.set_xlabel("Number of attempts")
+    cybench2.set_xlabel("Number of attempts")
+    intercode2.set_xlabel("Number of attempts")
+    cybench3.set_xlabel("Number of attempts")
+    intercode3.set_xlabel("Number of attempts")
+    cybench4.set_xlabel("Number of attempts")
+    intercode4.set_xlabel("Number of attempts")
 
     cybench1.set_ylabel("Success rate")
     intercode1.set_ylabel("Success rate")
-    cybench2.set_ylabel("Standard deviation")
-    intercode2.set_ylabel("Standard deviation")
+    cybench2.set_ylabel("Standard deviation (Success rate)")
+    intercode2.set_ylabel("Standard deviation (Success rate)")
     cybench3.set_ylabel("Success rate")
     intercode3.set_ylabel("Success rate")
-    cybench4.set_ylabel("Standard deviation")
-    intercode4.set_ylabel("Standard deviation")
+    cybench4.set_ylabel("Standard deviation (Success rate)")
+    intercode4.set_ylabel("Standard deviation (Success rate)")
 
     cybench1.set_title("Cybench")
     intercode1.set_title("IntercodeCTF")
@@ -117,10 +122,10 @@ def line_plot(data: dict) -> None:
     fig3.legend(loc="lower center", bbox_to_anchor=(0.5, -0.1), ncol=2, fontsize=12, frameon=False)
     fig4.legend(loc="lower center", bbox_to_anchor=(0.5, -0.1), ncol=2, fontsize=12, frameon=False)
 
-    fig1.suptitle("Figure 1: Success rate per Epoch (Magnified)")
-    fig2.suptitle("Figure 2: Standard Deviation per Epoch (Magnified)")
-    fig3.suptitle("Figure 3: Success rate per Epoch with Confidence Interval (Magnified)")
-    fig4.suptitle("Figure 2: Standard Deviation per Epoch (Magnified)")
+    fig1.suptitle("Figure 1: Success Rate over Number of Attempts (Magnified)")
+    fig2.suptitle("Figure 2: Standard Deviation of Success Rate over Number of Attempts (Magnified)")
+    fig3.suptitle("Figure 1: Success Rate over Number of Attempts (Magnified)")
+    fig4.suptitle("Figure 2: Standard Deviation of Success Rate over Number of Attempts (Magnified)")
 
     fig1.tight_layout(rect=(0, 0.05, 1, 1))
     fig2.tight_layout(rect=(0, 0.05, 1, 1))
@@ -132,8 +137,10 @@ def line_plot(data: dict) -> None:
     fig3.savefig("../results/mean_zoom_ci.png")
     fig4.savefig("../results/stdev_zoom_ci.png")
 
-    fig1.suptitle("Figure 1: Success rate per Epoch")
-    fig4.suptitle("Figure 2: Standard Deviation per Epoch")
+    fig1.suptitle("Figure 1: Success Rate over Number of Attempts")
+    fig2.suptitle("Figure 2: Standard Deviation of Success Rate over Number of Attempts")
+    fig3.suptitle("Figure 1: Success Rate over Number of Attempts")
+    fig4.suptitle("Figure 2: Standard Deviation of Success Rate over Number of Attempts")
 
     cybench1.set_ylim(0, 1)
     intercode1.set_ylim(0, 1)
@@ -175,11 +182,11 @@ def box_plot(data: dict) -> None:
     cybench.set_ylabel("Mean")
     intercode.set_ylabel("Mean")
     fig.suptitle("Figure 5: Box plot")
-    fig.savefig("results/boxplot_zoom.png")
+    fig.savefig("../results/boxplot_zoom.png")
 
     cybench.set_ylim(0, 1)
     intercode.set_ylim(0, 1)
-    fig.savefig("results/boxplot.png")
+    fig.savefig("../results/boxplot.png")
     return
 
 def _get_score(score) -> float:
@@ -190,27 +197,56 @@ def _get_score(score) -> float:
         return 0.0
     raise ValueError(f"Invalid score: {val}")
 
-def _compute_ci(y):
-    ci_lower = y - 1.96 * np.std(y) / np.sqrt(len(y))
-    ci_upper = y + 1.96 * np.std(y) / np.sqrt(len(y))
-    print("CI", ci_lower, ci_upper)
+def _compute_mean(name):
+    task_name, model_name = name.split("/")
+    task = "cybench" if "cybench" in task_name.lower() else "intercode"
+    model = "mistral" if "mistral" in model_name.lower() else "gemma"
+    df = pd.read_csv(f"../results/final_{model}_{task}_{num_of_epochs}_epochs.csv", index_col=0, header=0).iloc[:-1, :-1]
+
+    means = []
+    stdevs = []
+    for e in range(1, num_of_epochs+1):
+        sample = df.iloc[:, :e]
+        m = sample.mean(axis=1)
+        means.append(np.mean(m))
+        stdevs.append(np.std(m))
+    return means, stdevs
+
+def _compute_ci(y, std):
+    print(std)
+    ci_lower = y - 1.96 * np.array(std) / np.sqrt(len(y))
+    ci_upper = y + 1.96 * np.array(std) / np.sqrt(len(y))
+    # print("CI", ci_lower, ci_upper)
     return ci_lower, ci_upper
 
-def _compute_ci_bootstrap(data, epoch, n_bootstrap=100):
-    estimates = []
-    for _ in range(n_bootstrap):
-        sample = np.random.choice(data, size=epoch, replace=True)
-        estimates.append(np.std(sample))
-    ci_lower = np.mean(estimates)
-    ci_upper = np.mean(estimates)
-    return ci_lower, ci_upper
+def _compute_ci_bootstrap(name, epoch=num_of_epochs, n_bootstrap=100):
+    task_name, model_name = name.split("/")
+    task = "cybench" if "cybench" in task_name.lower() else "intercode"
+    model = "mistral" if "mistral" in model_name.lower() else "gemma"
+    df = pd.read_csv(f"../results/final_{model}_{task}_{num_of_epochs}_epochs.csv", index_col=0, header=0).iloc[:-1, :-1]
+
+    mean_stds = []
+    ci_lowers = []
+    ci_uppers = []
+    for e in range(num_of_epochs):
+        estimates = []
+        for _ in range(n_bootstrap):
+            attempts = np.random.choice(df.shape[1], size=e+1, replace=True)
+            sample = df.iloc[:, attempts]
+            means = sample.mean(axis=1)
+            estimates.append(np.std(means))
+        mean_stds.append(np.mean(estimates))
+        ci_lowers.append(np.percentile(estimates, 2.5))
+        ci_uppers.append(np.percentile(estimates, 97.5))
+    # print(name, mean_stds, ci_lowers, ci_uppers)
+    return mean_stds, ci_lowers, ci_uppers
 
 if __name__ == "__main__":
     data = extract()
-    print("\nDATA")
-    print(data)
+    # print("\nDATA")
+    # print(data)
     out = analyze(data)
-    for i in list(out.items()):
-        print(i)
+    # for i in list(out.items()):
+    #     print(i)
     line_plot(out)
-    box_plot(out)
+    # box_plot(out)
