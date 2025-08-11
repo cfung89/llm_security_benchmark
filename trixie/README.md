80c4abb3ea205251aaef7d9330e72bc95205107f
Cyrus <cyhfung@gmail.com>
2025-08-11 16:41:01 -0400

    chore: sync

---
+++ trixie/README.md
@@ -0,0 +1,5 @@
+# Trixie
+
+```
+alias ollama='/gpfs/projects/DT/cyber/inspect_experiments/ollama'
+```
--- trixie/install.sh
+++ trixie/install.sh
@@ -1,31 +1,27 @@
 #! /bin/bash
 
-read -p "Is Python environment already set up? [y/N]: " answer
+read -p "Is the Python environment already set up? [y/N]: " answer
 
 if [[ ! "$answer" =~ ^[yY]$ ]]; then
     # Confirm Python version & paths
     module load python/3.12.1-gcc-11.3.1-7tuhjhr
-    module list 
-    which python
-    which pip
+    module list
     python --version
 
-    read -p "Name of Python virtual environment: " venv_name
-    mkdir -p $HOME/venvs
-    if [ -d "$HOME/venvs/$venv_name" ]; then
-        rm -rf $HOME/venvs/$venv_name
-    fi
-    python3 -m venv $HOME/venvs/$venv_name
-    ln -s $HOME/venvs/$venv_name $venv_name
-    source $venv_name/bin/activate
+    read -p "Path where the Python virtual environment should be saved (e.g.: ~/venvs/.venv): " venv_name
+    expanded_dir=$(eval echo "$venv_name")
+
+    mkdir -p $expanded_dir
+    python3 -m venv $expanded_dir
+    source $expanded_dir
+
+    which python
+    which pip
 
     pip install -U pip
     pip install inspect_ai \
                 git+https://github.com/UKGovernmentBEIS/inspect_evals \
                 openai \
-                # vllm \
-                # mistral_common # Mistral-Large-Instruct-2411
-    # pip freeze > requirements.txt
     deactivate
     echo -e "\nSuccessfully installed Python packages to '$venv_name'."
 fi
@@ -34,9 +30,18 @@ read -p "Is Ollama already set up? [y/N]: " answer
 
 if [[ ! "$answer" =~ ^[yY]$ ]]; then
     # Manual installation of Ollama
+    read -p "Path where the Python virtual environment should be saved (e.g.: ~/bin/ollama): " dir
+    expanded_dir=$(eval echo "$dir")
+    mkdir -p $expanded_dir
+    cd $expanded_dir
+
     curl -LO https://ollama.com/download/ollama-linux-amd64.tgz
     tar -xzf ollama-linux-amd64.tgz
+
     mkdir -p $HOME/bin
-    mv ollama/bin/ollama $HOME/bin/ollama
-    echo -e "\nSuccessfully installed Ollama to '$HOME/bin/ollama'."
+    mv ollama/ ollama_temp/
+    mv ollama_temp/bin/ollama .
+    rm -rf ollama_temp ollama-linux-amd64.tgz
+
+    echo -e "\nSuccessfully installed Ollama to '$expanded_dir'."
 fi
